bbb3128e4e655ebcc99d52c2badac9cf7b22ffa7
Navraj Sikand <nonunavraj@gmail.com>
2025-01-10 17:34:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,78 @@
-function simulateTyping(textArea, text, button, delay = 100) {
-    let index = 0;
-
-    function typeCharacter() {
-        if (index < text.length) {
-            textArea.value += text[index];
-            textArea.dispatchEvent(new Event('input'));
-            index++;
-            setTimeout(typeCharacter, delay);
-        } else {
-            setTimeout(() => {
-                button.click();
-                button.dispatchEvent(new Event('click'));
-            }, 100);
-        }
-    }
-
-    typeCharacter();
-}
-
-const buttons = document.querySelectorAll(".show-answer-button");
-
-buttons.forEach((button, index) => {
-  setTimeout(() => {
-    button.click();
-    button.click();
-    console.log(`Clicked button ${index + 1}`);
-  }, index * 1000);
-});
-
 const radioButtons = document.querySelectorAll('input[type="radio"]');
 
 function clickRadioButtonsWithDelay(index = 0) {
-    if (index < radioButtons.length) {
-        radioButtons[index].click();
-        console.log(`Clicked radio button at index ${index}`);
+  if (index < radioButtons.length) {
+    radioButtons[index].click();
+    console.log(`Clicked radio button at index ${index}`);
 
-        setTimeout(() => clickRadioButtonsWithDelay(index + 1), 1000);
-    } else {
-        console.log("All radio buttons have been clicked.");
-    }
+    setTimeout(() => clickRadioButtonsWithDelay(index + 1), 1000);
+  } else {
+    console.log("All radio buttons have been clicked.");
+  }
 }
 
 clickRadioButtonsWithDelay();
 
-// --- Do the above then do htis one
+function simulateTyping(textArea, text, button, delay = 100) {
+  let index = 0;
 
+  function typeCharacter() {
+    if (index < text.length) {
+      textArea.value += text[index];
+      textArea.dispatchEvent(new Event("input"));
+      index++;
+      setTimeout(typeCharacter, delay);
+    } else {
+      setTimeout(() => {
+        button.click();
+        button.dispatchEvent(new Event("click"));
+      }, 100);
+    }
+  }
 
+  typeCharacter();
+}
 
-const questions = Array.from(
-  document.getElementsByClassName("short-answer-question"),
-);
-questions.forEach((question) => {
-  setTimeout(() => {
-    const qDiv = question.children[0];
-    const textInp =
-      qDiv.children[1].children[0].children[0].children[0].children[0];
-    const answer = question.children[1].children[1].children[0].innerText;
-    const check = qDiv.children[1].children[0].children[1].children[0];
+function executePart1() {
+  return new Promise((resolve) => {
+    const buttons = document.querySelectorAll(".show-answer-button");
+    buttons.forEach((button, index) => {
+      setTimeout(() => {
+        button.click();
+        button.click();
+        console.log(`Clicked button ${index + 1}`);
+        if (index === buttons.length - 1) {
+          resolve();
+        }
+      }, index * 1000);
+    });
+  });
+}
 
-    simulateTyping(textInp, answer, check ,100);
+async function executePart2() {
+  const questions = Array.from(
+    document.getElementsByClassName("short-answer-question"),
+  );
+  for (const question of questions) {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        const qDiv = question.children[0];
+        const textInp =
+          qDiv.children[1].children[0].children[0].children[0].children[0];
+        const answer = question.children[1].children[1].children[0].innerText;
+        const check = qDiv.children[1].children[0].children[1].children[0];
 
-    setTimeout(() => {}, 500);
-  }, 1000);
-});
+        simulateTyping(textInp, answer, check, 100);
+
+        setTimeout(() => resolve(), 500);
+      }, 1000);
+    });
+  }
+}
+
+async function main() {
+  await executePart1();
+  await executePart2();
+}
+
+main();
